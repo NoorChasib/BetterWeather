656272675d04f44bf726b411c02e47b363b70f5a
Noor Chasib <noorchasib@gmail.com>
2023-03-17 23:55:34 -0700

    create bottom weather portion

--- BetterWeather/Views/WeatherView.swift
+++ BetterWeather/Views/WeatherView.swift
@@ -62,13 +62,28 @@ struct WeatherView: View {
          VStack {
             Spacer()
 
-            VStack(alignment: .leading, spacing: 20) {}
-               .frame(maxWidth: .infinity, alignment: .leading)
-               .padding()
-               .padding(.bottom, 20)
-               .foregroundColor(Color(hue: 0.656, saturation: 0.787, brightness: 0.354))
-               .background(.white)
-               .cornerRadius(20, corners: [.topLeft, .topRight])
+            VStack(alignment: .leading, spacing: 20) {
+               Text("Weather Now")
+                  .bold()
+                  .padding(.bottom)
+
+               HStack {
+                  WeatherRow(logo: "thermometer", name: "Min temp", value: weather.main.tempMin.roundDouble() + "°")
+                  Spacer()
+                  WeatherRow(logo: "thermometer", name: "Max temp", value: weather.main.tempMax.roundDouble() + "°")
+               }
+               HStack {
+                  WeatherRow(logo: "wind", name: "Wind speed", value: weather.wind.speed.roundDouble() + " m/s")
+                  Spacer()
+                  WeatherRow(logo: "humidity", name: "Humidity", value: weather.main.humidity.roundDouble() + "%")
+               }
+            }
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .padding()
+            .padding(.bottom, 20)
+            .foregroundColor(Color(hue: 0.656, saturation: 0.787, brightness: 0.354))
+            .background(.white)
+            .cornerRadius(20, corners: [.topLeft, .topRight])
          }
       }
       .edgesIgnoringSafeArea(.bottom)

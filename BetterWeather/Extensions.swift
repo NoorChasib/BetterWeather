e785cea41a83bfcee1d8395ef2a0acb967d55ef8
Noor Chasib <noorchasib@gmail.com>
2023-03-17 23:32:57 -0700

    create extensions

---
+++ BetterWeather/Extensions.swift
@@ -0,0 +1,14 @@
+//
+//  Extensions.swift
+//  BetterWeather
+//
+//  Created by Noor Chasib on 2023-03-17.
+//
+
+import Foundation
+
+extension Double {
+   func roundDouble() -> String {
+      return String(format: "%.0f", self)
+   }
+}

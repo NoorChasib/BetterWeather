e28904ded1f3a64ea7e909a9476b4f3b6211490a
Noor Chasib <noorchasib@gmail.com>
2023-03-17 21:51:35 -0700

    Initial commit

---
+++ BetterWeather/BetterWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterWeatherApp.swift
+//  BetterWeather
+//
+//  Created by Noor Chasib on 2023-03-17.
+//
+
+import SwiftUI
+
+@main
+struct BetterWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
